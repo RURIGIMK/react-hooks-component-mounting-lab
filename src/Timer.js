a4117294c544ddd1d01a408d8361fcbb2346ab63
RURIGIMK <rurigimk@gmail.com>
2024-06-13 10:56:04 +0300

    Commit

--- src/Timer.js
+++ src/Timer.js
@@ -1,40 +1,35 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class Timer extends Component {
-  state = {
-    time: 0,
-    color: "#" + Math.floor(Math.random() * 16777215).toString(16)
-  };
-
-  // add your code here
-
-  render() {
-    const { time, color } = this.state;
-    return (
-      <section className="Timer" style={{ background: color }}>
-        <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
-        <aside className="mountText">Mounted</aside>
-        <small onClick={this.handleClose}>X</small>
-      </section>
-    );
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: 0
+    };
+    this.clockTick = this.clockTick.bind(this);
   }
 
-  //clock functions
-  clockTick = () => {
-    this.setState(prevState => ({
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  clockTick() {
+    this.setState((prevState) => ({
       time: prevState.time + 1
     }));
-  };
+  }
 
-  stopClock = () => {
-    clearInterval(this.interval);
-  };
-
-  // for the 'x' button,
-  handleClose = () => {
-    this.props.removeTimer(this.props.id);
-  };
+  render() {
+    return (
+      <div>
+        Timer: {this.state.time}
+      </div>
+    );
+  }
 }
 
 export default Timer;
